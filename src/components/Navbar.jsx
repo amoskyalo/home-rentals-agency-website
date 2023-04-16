8856adff4d20c63a0678f020c046b5142b779882
Olosko23 <oloogeorge633@gmail.com>
2023-04-16 08:55:23 +0300

    'test'

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Navbar = () => {
   return (
-    <div>
+    <div className="flex justify-between px-10 py-2">
       <h1>Logo</h1>
       <div>List</div>
     </div>
